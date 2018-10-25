d2cf2799f656c67f92372ffa0793dee21cbf286d
nikinjkeee <34586908+nikinjkeee@users.noreply.github.com>
2018-10-25 21:40:19 +0200

    uppdatering av fliken kontakta oss

---
+++ js/kontaktmail.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".jagvill1").click(function(){
+        $(".email").text("Email: kirungdomsforetag@gmail.com");
+        $(".namn").text("Namn: KIR-UF");
+        $(".roll").text("Roll: -");
+    });
+    $(".jagvill2").click(function(){
+        $(".email").text("Email: daniel.herrblom@gmail.com");
+        $(".namn").text("Namn: Daniel Herrblom");
+        $(".roll").text("Roll: kommunikatör");
+    });
+    $(".jagvill3").click(function(){
+        $(".email").text("Email: nikitamatveenko0909@gmail.com");
+        $(".namn").text("Namn: Nikita Matveenko");
+        $(".roll").text("Roll: försäljnings och webbansvarig");
+    });
+    $(".jagvill4").click(function(){
+        $(".email").text("Email: kirungdomsforetag@gmail.com");
+        $(".namn").text("Namn: KIR-UF");
+        $(".roll").text("Roll: -");
+    });
+});
