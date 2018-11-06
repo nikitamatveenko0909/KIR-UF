27dae7127997a3a32ec4810019c37090ae43e470
nikinjkeee <34586908+nikinjkeee@users.noreply.github.com>
2018-11-06 21:47:25 +0100

    stor uppdatering

--- js/privat_click.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function{
-    $("button").click(function{
-        window.location.href = "http://www.google.com";                    
-    });
-});
-// window.location.href = "http://www.google.com";
--- js/privat_foretag.js
+++ js/privat_foretag.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
     $(".kurser").click(function(){
         $(".skr").css("display", "inline");
-        $("#privat").click(function(){redirect("http://google.com");});
-        $("#foretag").click(function(){redirect("https://www.facebook.com/");});
+        $("#privat").click(function(){redirect("kurser_privat.html");});
+        $("#foretag").click(function(){redirect("kurser_foretag.html");});
     });
     $(".itsupport").click(function(){
         $(".skr").css("display", "inline");
-        $("#privat").click(function(){redirect("kurser_foretag.html");});
-        $("#foretag").click(function(){redirect("kurser_foretag.html");});
+        $("#privat").click(function(){redirect("itsupport_privat.html");});
+        $("#foretag").click(function(){redirect("itsupport_foretag.html");});
     });
     $(".radgivning").click(function(){
         $(".skr").css("display", "inline");
-        $("#privat").click(function(){redirect("kurser_foretag.html");});
-        $("#foretag").click(function(){redirect("kurser_foretag.html");});
+        $("#privat").click(function(){redirect("radgivning_privat.html");});
+        $("#foretag").click(function(){redirect("radgivning_foretag.html");});
     });
 });
 
